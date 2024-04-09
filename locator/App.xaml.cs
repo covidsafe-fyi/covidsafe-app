4de4466d85f04ca8c31cf19c7218adbea8082005
Rob Relyea <rob@relyeas.net>
2024-04-09 10:31:51 -0700

    maui app template

---
+++ locator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace locator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ locator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace locator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
