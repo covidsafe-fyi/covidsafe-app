0a7b82798db60a5055b34b73034750af40357148
Rob Relyea <rob@relyeas.net>
2024-05-07 10:28:09 -0700

    move Provider to library

--- locator/MauiProgram.cs
+++ locator/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui.Maps;
 
 namespace locator
 {
@@ -6,9 +7,12 @@ namespace locator
     {
         public static MauiApp CreateMauiApp()
         {
+            //key from dev account rob@bogle.tools
+
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkitMaps("fkYHCmIsjNDXuOe28mvw~TAkfJSkQ-xmZl2P5Mfs_LA~AkCmFvJJ8Ei1DlY_ykD5hQyzPYTw0AeoVvCHUdoZWtKnXg9m50q7ZX1tjPamoXDi")
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
